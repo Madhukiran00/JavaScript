dde852c9703c3f9e01f8bfbe0ef33de018479d1e
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-05-31 14:04:33 +0530

    js

--- first.js
+++ first.js
@@ -404,20 +404,20 @@
 
 
 
-debugger;
-let color="blue";
-switch(color){
-    case "red":
-        console.log("The color is red")
-        break;
-    case "yello":
-        console.log("The color is yello")
-        break;
-    case "blue":
-        console.log("The color is blue")
-        break;
-    default: "The color is not available";
-   }
+// debugger;
+// let color="blue";
+// switch(color){
+//     case "red":
+//         console.log("The color is red")
+//         break;
+//     case "yello":
+//         console.log("The color is yello")
+//         break;
+//     case "blue":
+//         console.log("The color is blue")
+//         break;
+//     default: "The color is not available";
+//    }
 
 
 
--- javascript.js
+++ javascript.js
@@ -1,14 +1,21 @@
- debugger;
-let color="blue";
-switch(color){
-    case "red":
-        console.log("The color is red")
-        break;
-    case "yello":
-        console.log("The color is yello")
-        break;
-    case "blue":
-        console.log("The color is blue")
-        break;
-    default: "The color is not available";
-   }
+//  debugger;
+// let color="blue";
+// switch(color){
+//     case "red":
+//         console.log("The color is red")
+//         break;
+//     case "yello":
+//         console.log("The color is yello")
+//         break;
+//     case "blue":
+//         console.log("The color is blue")
+//         break;
+//     default: "The color is not available";
+//    }
+
+let i=1
+do{
+    console.log(i);
+    i++;
+
+}while(i<10);
