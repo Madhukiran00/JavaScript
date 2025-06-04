723c8667aab6ad59dde089eeb7febec8ff85d2a1
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-06-04 08:13:10 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -46,6 +46,31 @@
 
 
 
-let arr=[1,2,3,4,5,6,7,8,9,10,,,,,,,,,];
+// let arr=[1,2,3,4,5,6,7,8,9,10,,,,,,,,,];
 
-console.log(arr[0]);
+// console.log(arr[0]);
+
+
+// let my_name="MadhuKiran"
+
+// console.log(my_name.toUpperCase())
+// console.log(my_name.toLowerCase())
+// console.log(my_name.slice(0,-4))
+// console.log(my_name.substring(0,4))
+// console.log(my_name.substr(2,2)) 
+// console.log(my_name.padEnd(11,"S"));
+// console.log(my_name.padStart(15,"d"));  
+// let s1="This is string"
+// console.log(s1.replace("is","$$n"));
+ 
+let arr=[1,2,3,4,5]
+
+delete arr[2]
+console.log(arr);
+arr.slice(2)
+console.log(arr);
+ 
+
+
+
+
