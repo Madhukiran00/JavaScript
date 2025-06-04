7418a8a162aa7faa010db6e9f2bd728d64e93941
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-06-04 10:22:43 +0530

    Update javascript.js

--- 03-06-25/javascript.js
+++ 03-06-25/javascript.js
@@ -49,6 +49,40 @@ let arr2=[4,9,1,8,2,5,3]
 console.log(arr2.slice(2))
 console.log(arr2.slice(2,5))
 
-arr2=arr2.split(",")
+let arr3=["4","9","1","8","2","5","3"]
+console.log(arr2.join())
+
+arr2=[4,9,1,8,2,5,3,8,4,7]
+console.log(arr2.indexOf(8))
+console.log(arr2.lastIndexOf(8))
+console.log(arr2.includes(8))
+
+
+var arr4=[2,6,8,6,4,7,5]
+var result=arr4.find(fun)
+console.log(result)
+
+function fun(value){
+    return value>4
+}
+
+let res=arr4.filter(fun1)
+console.log(res)
+
+function fun1(value){
+    return value>4
+}
+
+
+let res1=arr4.map(mul)
+console.log(res1)
+
+function mul(val){
+    return  val*2
+
+}
+
+
+
 
 
