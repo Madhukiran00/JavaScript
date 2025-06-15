563dd4db676a10e6cd7e52c16302d67da3ad3fcc
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-06-15 23:41:34 +0530

    js

--- 12-06-25/javascript.js
+++ 12-06-25/javascript.js
@@ -1,12 +1,12 @@
-let heros=["prabhas","ranveer","salman","sharukh","akshay"]
-let heroines=["deepika","katrina","kareena","shraddha","anushka"]
+let heros = ["prabhas", "ranveer", "salman", "sharukh", "akshay"]
+let heroines = ["deepika", "katrina", "kareena", "shraddha", "anushka"]
 
-let lis=document.createElement('ul')
-for (let i=0;i<heros.length;i++){
-    let li=document.createElement("li")
-    li.innerText=`${heros[i]} - ${heroines[i]}`
+let lis = document.createElement('ul')
+for (let i = 0; i < heros.length; i++) {
+    let li = document.createElement("li")
+    li.innerText = `${heros[i]} - ${heroines[i]}`
     lis.append(li)
-    li.style.listStyle="none"
-    
+    li.style.listStyle = "none"
+
 }
 document.body.append(lis)
