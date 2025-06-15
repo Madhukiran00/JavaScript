a77e04f53d052c70c273c3bde6c00f87460066a8
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-06-15 16:32:46 +0530

    js

--- first.js
+++ first.js
@@ -519,7 +519,13 @@
 let heros=["prabhas","ranveer","salman","sharukh","akshay"]
 let heroines=["deepika","katrina","kareena","shraddha","anushka"]
 
+let lis=document.createElement('ul')
 for (let i=0;i<heros.length;i++){
-    console.log(heros[i],"-",heroines[i])
+    let li=document.createElement("li")
+    li.innerText=`${heros[i]} - ${heroines[i]}`
+    lis.append(li)
+    li.style.listStyle="none"
+    
 }
+document.body.append(lis)
 
