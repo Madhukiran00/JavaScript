2e38885ae3bf58ef771030c614cf837477d9cde2
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-06-16 12:33:02 +0530

    js

---
+++ 14-06-25/javascript.js
@@ -0,0 +1,12 @@
+
+let heading=document.createElement("h1")
+
+heading.innerText="products"
+document.body.append(heading)
+heading.style.textDecoration="underline"
+heading.style.textTransform="Capitalize"
+heading.style.textAlign="center"
+
+let seperator=document.createElement("hr")
+document.body.append(seperator)
+
