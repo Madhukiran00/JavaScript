b23dda7820aec5f1182b0bf67f745e0ff2624794
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-06-19 15:53:39 +0530

    Update first.js

--- first.js
+++ first.js
@@ -516,16 +516,19 @@
 
 
 
-let heros=["prabhas","ranveer","salman","sharukh","akshay"]
-let heroines=["deepika","katrina","kareena","shraddha","anushka"]
+// let heros=["prabhas","ranveer","salman","sharukh","akshay"]
+// let heroines=["deepika","katrina","kareena","shraddha","anushka"]
 
-let lis=document.createElement('ul')
-for (let i=0;i<heros.length;i++){
-    let li=document.createElement("li")
-    li.innerText=`${heros[i]} - ${heroines[i]}`
-    lis.append(li)
-    li.style.listStyle="none"
+// let lis=document.createElement('ul')
+// for (let i=0;i<heros.length;i++){
+//     let li=document.createElement("li")
+//     li.innerText=`${heros[i]} - ${heroines[i]}`
+//     lis.append(li)
+//     li.style.listStyle="none"
     
-}
-document.body.append(lis)
+// }
+// document.body.append(lis)
+
+
+window.
 
