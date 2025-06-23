a6e5c99f7cb18894d073de4799ea1abc8fc59b9c
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-06-23 16:30:58 +0530

    js

--- 16-06-25/javascript.js
+++ 16-06-25/javascript.js
@@ -1,1 +1,11 @@
-let 
+let inp1=document.getElementById("input1")
+
+let inp2=document.getElementById("input2")
+
+let btn=document.getElementById("button1")
+
+btn.addEventListener("click",function(){
+    let input1=inp1.value
+    let input2=inp2.value
+    console.log(input1+input2)
+})
