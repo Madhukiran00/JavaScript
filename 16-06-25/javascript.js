7bb3537f1da2cc72e3f67e694abd3d986f1a791c
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-06-23 16:33:20 +0530

    Update javascript.js

--- 16-06-25/javascript.js
+++ 16-06-25/javascript.js
@@ -5,7 +5,7 @@ let inp2=document.getElementById("input2")
 let btn=document.getElementById("button1")
 
 btn.addEventListener("click",function(){
-    let input1=inp1.value
-    let input2=inp2.value
+    let input1=+(inp1.value)
+    let input2=+(inp2.value)
     console.log(input1+input2)
 })
