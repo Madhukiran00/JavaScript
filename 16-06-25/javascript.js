0a0a35c564dc0c150b04deafae9567722c39a5c6
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-06-23 16:45:12 +0530

    js

--- 16-06-25/javascript.js
+++ 16-06-25/javascript.js
@@ -8,6 +8,8 @@ btn.addEventListener("click",function(){
     let input1=+(inp1.value)
     let input2=+(inp2.value)
     console.log(input1+input2)
+    let sum=input1+input2
+    document.getElementById("output").value=sum
     
 })
 document.input.append(input1+input2)
