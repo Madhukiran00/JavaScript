58685464355e6c7029470ac38a00f679e10fe05a
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-06-28 15:48:48 +0530

    Update first.js

--- first.js
+++ first.js
@@ -553,41 +553,41 @@
 
 
 
-let grandparent=document.getElementById("grandparent")
- grandparent.style.height="400px"
- grandparent.style.width="400px"
- grandparent.style.border="2px solid red"
+// let grandparent=document.getElementById("grandparent")
+//  grandparent.style.height="400px"
+//  grandparent.style.width="400px"
+//  grandparent.style.border="2px solid red"
  
 
-grandparent.addEventListener("click",(e)=>{
-    console.log("This Is Grand Parent")
-    // e.stopPropagation(); 
+// grandparent.addEventListener("click",(e)=>{
+//     console.log("This Is Grand Parent")
+//     // e.stopPropagation(); 
 
- })
+//  })
 
- let parent=document.getElementById("parent")
+//  let parent=document.getElementById("parent")
 
- parent.style.height="300px"
- parent.style.width="300px"
- parent.style.border="2px solid blue"
+//  parent.style.height="300px"
+//  parent.style.width="300px"
+//  parent.style.border="2px solid blue"
 
- parent.addEventListener("click",(e)=>{
-    console.log("This Is Parent")
-    // e.stopPropagation(); 
- },true)
+//  parent.addEventListener("click",(e)=>{
+//     console.log("This Is Parent")
+//     // e.stopPropagation(); 
+//  },true)
 
 
 
-let child=document.getElementById("child")
-child.style.height="200px"
-child.style.width="200px"
-child.style.border="2px solid green"
+// let child=document.getElementById("child")
+// child.style.height="200px"
+// child.style.width="200px"
+// child.style.border="2px solid green"
 
-child.addEventListener("click",(e)=>{
-    console.log("This Is Child")
-    // e.stopPropagation(); 
+// child.addEventListener("click",(e)=>{
+//     console.log("This Is Child")
+//     // e.stopPropagation(); 
 
-},true)
+// },true)
 
 
 
