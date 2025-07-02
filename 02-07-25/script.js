5602bf8b8ed7d7499ab10f10c1ba3d2a129447f1
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-07-02 13:37:23 +0530

    js

--- 02-07-25/script.js
+++ 02-07-25/script.js
@@ -1,11 +1,28 @@
-let user_name=getElementById("name").value
-let mobile=getElementById("mobile").value
-let pass=getElementById("pass").value
+let user_name = getElementById("name").value
+let mobile = getElementById("mobile").value
+let pass = getElementById("pass").value
+let email = getElementById("email").value
 
-obj={
-    user_name:user_name
+
+
+button("click", () => {
+
+
+    details = {
+    user_name: use_name,
+    mobile: mobile,
+    pass: pass,
+    email: email
 }
 
+
+    fetch("https://localhost/users", {
+        method: "POST",
+        headers: { "Content-Type": "Application/json" },
+        body: JSON.stringify(details)
+    })
+
+})
 
 
 
