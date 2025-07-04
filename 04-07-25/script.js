0d1ff48f323dd4c4464788231aea0e60067ac66d
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-07-04 12:24:02 +0530

    Create script.js

---
+++ 04-07-25/script.js
@@ -0,0 +1,43 @@
+a=[1,2,3]
+// u=JSON.stringify(a)
+// a.pop()
+// b=JSON.parse(u)
+// console.log(a,b)
+
+
+// function add(...a){
+//     let sum=0
+//     if (a.length>0){
+//         for (let i=0;i<a.length;i++){
+//             sum=sum+a[i]
+//         }
+    
+//     }
+//     return sum
+    
+// }
+// console.log(add(1,2,3,4,5))
+
+
+
+
+
+
+
+// function add(...a){
+//     let sum=0
+//     if (a.length>0){
+//         for (let i=0;i<a.length;i++){
+//             sum=sum+a[i]
+//         }
+    
+//     }
+//     return sum
+    
+// }
+// console.log(add(1,2,3,4,5))
+
+
+
+
+
