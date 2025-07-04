0816e0877b422636963a611f5c055143f45a57de
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-07-04 12:54:12 +0530

    Update script.js

--- 04-07-25/script.js
+++ 04-07-25/script.js
@@ -1,4 +1,4 @@
-a=[1,2,3]
+// a=[1,2,3]
 // u=JSON.stringify(a)
 // a.pop()
 // b=JSON.parse(u)
@@ -38,6 +38,18 @@ a=[1,2,3]
 // console.log(add(1,2,3,4,5))
 
 
+// #Destructuring 
+// l=[1,2,3]
+// const [a,b,c]=l
+// console.log(a,b,c)
+// const [a,b,c] = [10, 20, 30];
+// console.log(a,b,c);  // 10 30
 
+// const l = [1, 2];
+// const [a, , c=6] = l;
+// console.log(a,c);
 
+// a={name:"madhu",age:"21"}
+
+// console.warn("sdjkdf")
 
