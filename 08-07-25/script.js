11075273ad73c1c6896a23cff3aa384df38e13fc
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-07-08 09:43:27 +0530

    js

---
+++ 08-07-25/script.js
@@ -0,0 +1,15 @@
+let startBtn=document.getElementById("startBtn")
+let stopBtn=document.getElementById("stopBtn")
+let resetBtn=document.getElementById("resetBtn")
+
+startBtn.onclick=function(){
+
+}
+
+stopBtn.onclick=function(){
+
+}
+
+resetBtn.onclick=function(){
+
+}
