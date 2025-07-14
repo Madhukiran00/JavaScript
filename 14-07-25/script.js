4d5e1286e2ccca3008713ec544995a62f58c7f1d
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-07-14 19:44:44 +0530

    Update script.js

--- 14-07-25/script.js
+++ 14-07-25/script.js
@@ -1,118 +1,120 @@
 
-// let url="https://dummyjson.com/users"
 
-// let data=fetch(url)
-// .then((da)=>{
-//     console.log(da)
-// }).catch((err)=>{
+
+// let data=fetch("https://dummyjson.com/users")
+// .then((da)=>(da.json()))
+// .then((da)=>console.log(da)).catch((err)=>{
 //     console.log("Error:", err)
 // })
 
 
 
 
-// myPromise= new Promise((resolve, reject) =>{
-//     let is_delivered=true;
-//     if(is_delivered){
-//         resolved("Order is delivered");
-//     }else{
-//         reject("Order is not delivered");
-//     }
-// })
-// mypromise.then((msg)=>{
-//     console.log(msg);
-// }).catch((err)=>{
-//     console.log(err);
-// })
-
-
-
-
-
-
-
-
-
-// myPromise= new Promise((resolved, reject) =>{
-//     let num=4;
-//     if(num%2==0){
-//         resolved("Even");
-//     }else{
-//         reject("Odd");
-//     }
-// })
-// myPromise.then((msg)=>{
-//     console.log(msg);
-// }).catch((err)=>{
-//     console.log(err);
-// })
-
-
-
-
-// myPromise= new Promise((resolved, reject) =>{
-// let companyDetails=[{companycode:"ABC123",employId:[101,102,103,104]},
-// {companycode:"EFG123",employId:[201,202,203,204]},
-// {companycode:"HIJ123",employId:[301,302,303,304]},
-// {companycode:"KLM123",employId:[401,402,403,404]},
-// {companycode:"NOP123",employId:[501,502,503,504]}]
-// let code="ABC123";
-// let id=103;
-//     for(let i=0; i<companyDetails.length; i++){
-//         if(companyDetails[i].companycode===code){
-//             code=companyDetails[i].companycode;
-//             break;
-//         }
-//     }
-   
-//     if(companyDetails.includes(code) && companyDetails[0].employId.includes(id)){
-//         resolved("Employee Found");
-//     }else{
-//         reject("Employee Not Found");
-//     }
-// })
-// myPromise.then((msg)=>{
-//     console.log(msg);
-// }).catch((err)=>{
-//     console.log(err);
-// })
-
-
-
-
-let companyDetails=[{companycode:"ABC123",employId:[101,102,103,104]},
-{companycode:"EFG123",employId:[201,202,203,204]},
-{companycode:"HIJ123",employId:[301,302,303,304]},
-{companycode:"KLM123",employId:[401,402,403,404]},
-{companycode:"NOP123",employId:[501,502,503,504]}]
-
-code="ABC123"
-
-for(let i=0; i<companyDetails.length; i++){
-    if(code=companyDetails[i].companycode==code){
-        
+let mypromise= new Promise((resolve, reject) =>{
+    let is_delivered=false;
+    if(is_delivered){
+        resolve("Order is delivered");
+    }else{
+        reject("Order is not delivered");
     }
-    
+})
+mypromise.then((msg)=>{
+    console.log(msg);
+}).catch((err)=>{
+    console.log(err);
+})
+
+
+
+
+myPromise= new Promise((resolved, reject) =>{
+    let num=4;
+    if(num%2==0){
+        resolved("Even");
+    }else{
+        reject("Odd");
+    }
+})
+myPromise.then((msg)=>{
+    console.log(msg);
+}).catch((err)=>{
+    console.log(err);
+})
+
+
+
+
+let companyDetails = [
+    { companycode: "TCS", employid: [101, 102, 103, 104] },
+    { companycode: "INF", employid: [201, 202, 203, 204] },
+    { companycode: "WIP", employid: [301, 302, 303, 304] },
+    { companycode: "HCL", employid: [401, 402, 403, 404] }]
+
+let code = "HCL";
+let id = 601;
+let details
+
+
+let promise=new Promise((resolve,reject)=>{
+
+for(let i=0; i<companyDetails.length;i++){
+    if (companyDetails[i].companycode==code){
+        details=companyDetails[i]
+    }
+}
+if (details!==undefined){
+    if (details.employid.includes(id)){
+        resolve("Employee exists")
+
+    }else{
+        reject("Employee does not exist")
+    }
+}else{
+    reject("Company does not exist")
 }
 
-// name
-// email
-// password
-// is_reistered=TRUE
+}).then((msg)=>{console.log(msg)})
+.catch((err)=>{console.log(err)})
+
+
 
 
 userDetails=[
     {"username":"madhu",
     "email":"madhu@gmail.com",
-    "password":"123456"},
-    {"username":"Madhu",
-    "email":"madhu@gmail.com",
-    "password":"123456"}]
+    "password":"123456789"},
 
+    {"username":"Madhukiran",
+    "email":"madhukiran@gmail.com",
+    "password":"987654321"}]
+
+let myPromise=new Promise((resolve,reject)=>{
     nam="madhu"
     mail="madhu@gmail.com"
+    pass="123456"
+let data;
 for(let i=0;i<userDetails.length;i++){
     if (userDetails[i].username == nam){
-        console.log("user exist")
+        data=userDetails[i]
     }
 }
+if (data!==undefined){
+    if(data.email==mail){
+        if(data.password==pass){
+            resolve("Login successsful");
+        }else{
+            reject("Incorrect password");
+        }
+    }else{
+        reject("Email not found");
+    }
+}else{
+    reject("User not found");
+}
+
+})
+myPromise.then((msg)=>{
+    console.log(msg)
+}).catch((err)=>{
+    console.log(err)
+})
