91e1bcb8d999b4a3fe29e1920bc2cad4fb1d73d2
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-07-23 14:56:33 +0530

    js

---
+++ 23-07-25/script.js
@@ -0,0 +1,18 @@
+// let obj={"first":"madhu","secound":"kiran","fullname":function fullname(){
+//     return (`${this.first} ${this.secound}`)
+// }}
+
+
+// console.log(obj.fullname())
+
+
+// "use strict"
+// function fun(){
+//     console.log(this)
+// }
+// fun()
+
+
+for (var i=0;i<5;i++){
+
+}
